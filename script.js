02b2110a79ca5df6c97e06f2982c3cbe0c46fb87
Codingluxo <luis.garcia@ug.uchile.cl>
2022-05-03 04:16:53 -0400

    Erase button added, I'm figuring out how can I generate a new grid in the same total space

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const container = document.querySelector("#container");
+let grids;
+let gridver;
 
     
 // Grid Creation
-    for(let i = 1; i<= 8; i++){
+    for(let i = 1; i<= 16; i++){
     
-    let grids = document.createElement("div");
+    grids = document.createElement("div");
     grids.setAttribute("data-number",`${i}`);
-            for(let j = 1; j<= 8; j++){
-                let gridsver = document.createElement("div");
+            for(let j = 1; j<= 16; j++){
+                gridsver = document.createElement("div");
                 gridsver.setAttribute("data-ver",`${j}`);
                 grids.appendChild(gridsver);
             }
@@ -19,12 +21,23 @@ const container = document.querySelector("#container");
 const hovering = document.querySelectorAll("[data-ver]");
 
 hovering.forEach((valor) => {
-    valor.addEventListener("mouseenter",(e)=>{
-        console.log(e.target.style.backgroundColor = `#${1 + Math.round(Math.random()*100)}${1 + Math.round(Math.random()*100) }${1 + Math.round(Math.random()*100) }`)
-    })
+    valor.addEventListener("mouseenter",(e)=>{ //random color RGB FORMAT
+            e.target.style.backgroundColor = `#${1 + Math.round(Math.random()*100)}${1 + Math.round(Math.random()*100) }${1 + Math.round(Math.random()*100) }`;            
 })
+});
 
+//Reset Button function
 
+const button = document.createElement("button");
+const body = document.querySelector("body");
+
+body.appendChild(button);
+
+button.addEventListener("click",()=>{
+    hovering.forEach(a =>{
+        a.style.backgroundColor = "#FFFFFF";
+    } )
+})
 
 
 
