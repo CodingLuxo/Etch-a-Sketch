44c64561625a8d9011daf51f62c9ff7cd344fb9a
Codingluxo <luis.garcia@ug.uchile.cl>
2022-05-03 02:22:57 -0400

    Setting the hover function

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const container = document.querySelector("#container");
-let grids;
-let gridsver;
+
     
 // Grid Creation
-    for(let i = 1; i<= 64; i++){
+    for(let i = 1; i<= 8; i++){
     
-    grids = document.createElement("div");
+    let grids = document.createElement("div");
     grids.setAttribute("data-number",`${i}`);
-            for(let j = 1; j<= 64; j++){
-                gridsver = document.createElement("div");
+            for(let j = 1; j<= 8; j++){
+                let gridsver = document.createElement("div");
                 gridsver.setAttribute("data-ver",`${j}`);
                 grids.appendChild(gridsver);
             }
     container.appendChild(grids);
     }
 
+// hover effect
 
+const hovering = document.querySelectorAll("[data-ver]");
+
+hovering.forEach((valor) => {
+    valor.addEventListener("mouseenter",(e)=>{
+        console.log(e.target.style.backgroundColor = `#${1 + Math.round(Math.random()*100)}${1 + Math.round(Math.random()*100) }${1 + Math.round(Math.random()*100) }`)
+    })
+})
 
 
 
